28c6dd5bcf789dac33ed31e6f7e389d501067eaf
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2021-01-17 16:14:34 +0100

    project structure

---
+++ affluencer/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav>
+		<div id="logo">
+			Affluencer
+			<img alt="Affluencer logo" src="../assets/images/logo.png">
+		
+	</nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+}
+</script>
--- affluencer/src/views/Home.vue
+++ affluencer/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<navbar/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import navbar from '@/components/Navbar.vue'
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    HelloWorld
+    navbar
   }
 }
 </script>
