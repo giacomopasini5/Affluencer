21079c58d83fe509e934a3453a387a58c000c4fd
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2021-06-14 17:53:47 +0200

    better list styling

--- affluencer/src/components/StoreAnnouncements.vue
+++ affluencer/src/components/StoreAnnouncements.vue
@@ -1,6 +1,6 @@
 <template>
-	<v-card v-if="!$store.state.config.settings" outlined class="text-left mr-10">
-		<v-card v-if="isOwner" outlined class="ma-5">
+	<v-card v-if="!$store.state.config.settings" flat class="text-left mr-10">
+		<v-card v-if="isOwner" outlined>
 			<v-card-title>Scrivi un annuncio</v-card-title>
 			<v-text-field label="Titolo" hide-details="auto" outlined dense class="ma-5"></v-text-field>
 			<v-textarea v-model="announcementText" label="Annuncio" rows="1" hide-details="auto" outlined dense auto-grow clearable class="ma-5"></v-textarea>
@@ -10,7 +10,7 @@
 				</v-btn>
 			</v-card-actions>
 		</v-card>
-		<v-card outlined class="ma-5">
+		<v-card outlined class="mt-2">
 			<v-card-title>{{ latestPost.title }}</v-card-title>
 			<v-card-subtitle>{{ latestPost.datetime }}</v-card-subtitle>
 			<v-card-text>{{ latestPost.text }}</v-card-text>
@@ -20,12 +20,12 @@
 				</v-btn>
 			</v-card-actions>
 		</v-card>
-		<v-list outlined class="pa-0 ma-5">
+		<v-list class="pa-0 mt-2">
 			<v-list-group>
 				<template v-slot:activator>
 					<v-list-item-title>Mostra più annunci</v-list-item-title>
 				</template>
-				<v-card v-for="post in otherPosts" :key="post.datetime" outlined class="ma-5 text-left">
+				<v-card v-for="post in otherPosts" :key="post.datetime" outlined class="mt-2">
 					<v-card-title>{{ post.title }}</v-card-title>
 					<v-card-subtitle>{{ post.datetime }}</v-card-subtitle>
 					<v-card-text>{{ post.text }}</v-card-text>
