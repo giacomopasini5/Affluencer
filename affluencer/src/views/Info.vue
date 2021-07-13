40ed26e911731503f54281e04601f71a434eacf3
Giacomo Pasini <giacomo.pasini5@studio.unibo.it>
2021-07-13 14:07:37 +0200

    info

--- affluencer/src/views/Info.vue
+++ affluencer/src/views/Info.vue
@@ -1,3 +1,31 @@
 <template>
-	<h1>INFO</h1>
+	<v-card elevation="5" class="ma-10">
+		<v-card-title>Componenti gruppo</v-card-title>
+		<v-card-text>
+			Michele Durante (michele.durante3@studio.unibo.it)<br>
+			Daniele Gambaletta (daniele.gambaletta@studio.unibo.it)<br>
+			Giacomo Pasini (giacomo.pasini5@studio.unibo.it)
+		</v-card-text>
+		<v-card-title>Progetto</v-card-title>
+		<v-card-text>
+			Sistema per la gestione degli accessi, comprendente recensioni e avvisi tra negozi e clienti.<br><br><br>
+			Negozio<br><br>
+			Il negozio si registra sul sito e fornisce periodicamente i dati sull'affluenza (persone all'interno e in coda),
+			comunicati tramite un apposito sensore simulato. La pagina del negozio comprende grafici che mostrano l'affluenza
+			in tempo reale ricevuta dal sensore, insieme a statistiche relative ad un periodo di tempo selezionabile.
+			Il grafico sull'affluenza integra anche le segnalazioni fatte dagli utenti, opportunamente distinte dai dati sensoriali.
+			Il negozio ha anche la possibilità di gestire una propria pagina personale sul sito comprensiva di bacheca per annunci al pubblico.
+			La pagina del negozio comprende anche una sezione per le recensioni dei clienti, a cui il negozio può rispondere.<br><br><br>
+			Cliente<br><br>
+			Un cliente può visitare il sito e ricercare un negozio, sia tramite una funzione di ricerca che attraverso una mappa, e visualizzarne la pagina.
+			Registrando un proprio account con informazioni di base, un utente può salvare i propri negozi preferiti in una lista per facilitarne la ricerca. 
+			n utente può scegliere di prenotare l'accesso ad un negozio per un dato orario. Un utente riceve notifiche per i reminder delle prenotazioni effettuate,
+			oppure se si trova nelle vicinanze di un negozio preferito che è in una situazione di bassa/alta affluenza. Un utente può inviare una propria
+			segnalazione sull'affluenza, che viene accettata se si trova nelle vicinanze del negozio. Un utente può eventualmente lasciare una recensione sulla pagina del negozio.<br><br><br>
+			Features real-time<br><br>
+			Ricezione dei dati in real-time relativi all'affluenza, dai sensori e dagli utenti. Sistema di notifiche relative alle prenotazioni effettuate e alla situazione dei negozi.<br><br><br>
+			Tecnologie utilizzate<br><br>
+			Solution stack MEVN
+		</v-card-text>
+	</v-card>
 </template>
